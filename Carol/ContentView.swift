bd97e5c93978394c7efb1ef2a11f7e5043f7a2e0
Anagh Sharma <anaghsharma12@gmail.com>
2019-10-13 01:25:27 +0530

    Initial Commit

---
+++ Carol/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Carol
+//
+//  Created by Anagh Sharma on 13/10/19.
+//  Copyright © 2019 Golden Chopper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
