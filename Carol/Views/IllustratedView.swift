c12c342f1c9106c51fe6610ab0292612a9918644
Anagh Sharma <anaghsharma12@gmail.com>
2019-11-09 17:51:59 +0530

    Added illustrations for different states

--- Carol/Views/IllustratedView.swift
+++ Carol/Views/IllustratedView.swift
@@ -12,21 +12,24 @@ struct IllustratedView: View {
     var illustrationImageName: String
     var illustrationText: String
     var body: some View {
-        VStack {
+        VStack(spacing: -12.0) {
             Image(illustrationImageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 256.0, height: 256.0)
             Text("\(illustrationText)")
                 .font(.footnote)
+                .fontWeight(.medium)
+                .foregroundColor(Color(NSColor.secondaryLabelColor))
                 .multilineTextAlignment(.center)
                 .frame(maxWidth: 224)
         }
     }
 }
 
-//struct IllustratedView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        IllustratedView()
-//    }
-//}
+struct IllustratedView_Previews: PreviewProvider {
+    static var previews: some View {
+        IllustratedView(illustrationImageName: "IllustrationNoApp", illustrationText: "No music app is running. Play some music from one of the apps.")
+            .frame(width: 360.0, height: 360.0)
+    }
+}
